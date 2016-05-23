02ca49c09c91612fef082dc2765ed6e57cbe4aa4
Yomi Colledge <baphled@boodah.net>
2016-05-23 17:46:07 +0100

    Refactor Password

--- lib/password.rb
+++ lib/password.rb
@@ -2,31 +2,24 @@ class Password
   def self.generate(length=8, options={lowercase: true, uppercase: true, numeric: true, special: true})
     password = []
 
-    password_combo = []
-    if options[:uppercase]
-      password_combo.concat(uppercase)
-    end
-
-    if options[:lowercase]
-      password_combo.concat(lowercase)
-    end
-
-    if options[:numeric]
-      password_combo.concat(numeric)
-    end
-
-    if options[:special]
-      password_combo.concat(special)
-    end
-
-    length.times do
-      password << password_combo[rand(password_combo.size)]
-    end
+    characters = password_characters(options)
+    length.times { password << characters[rand(characters.size)] }
 
     password.join
   end
 
   protected
+
+  def self.password_characters(options)
+    password_characters = []
+
+    password_characters.concat(uppercase) if options[:uppercase]
+    password_characters.concat(lowercase) if options[:lowercase]
+    password_characters.concat(numeric) if options[:numeric]
+    password_characters.concat(special) if options[:special]
+
+    password_characters
+  end
 
   def self.lowercase
     ('a'..'z').to_a
